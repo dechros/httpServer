de89cd110361562fa86aec8b8cea2426fd269bab
Halit <halit.cetin@segulagrp.de>
2024-03-13 09:05:10 +0100

    multiple server configuration is added.

--- source/main.c
+++ source/main.c
@@ -14,21 +14,38 @@
 
 #define PORT 8888
 
-static struct tcp_server server;
+static struct tcp_server server1;
+static struct tcp_server server2;
 
 int main(void)
 {
     enum server_errors error;
-    error = server_init(&server, PORT);
+    error = server_init(&server1, PORT);
     if (error != SERVER_NO_ERROR)
     {
         return EXIT_FAILURE;
     }
 
-    error = server_run(&server);
+    error = server_init(&server2, 3152);
     if (error != SERVER_NO_ERROR)
     {
         return EXIT_FAILURE;
+    }
+
+    error = server_run(&server1);
+    if (error != SERVER_NO_ERROR)
+    {
+        return EXIT_FAILURE;
+    }
+
+    error = server_run(&server2);
+    if (error != SERVER_NO_ERROR)
+    {
+        return EXIT_FAILURE;
+    }
+
+    while (true)
+    {
     }
 
     return EXIT_SUCCESS;
--- source/server.c
+++ source/server.c
@@ -5,10 +5,10 @@
 #include "server.h"
 #include "client.h"
 
+static void *server_task(void *arg);
+
 enum server_errors server_init(struct tcp_server *tcp_server_data, const int port)
 {
-    enum server_errors ret_val = SERVER_INIT_ERROR;
-
     tcp_server_data->server_address.sin_family = AF_INET;         /* IPV4 */
     tcp_server_data->server_address.sin_addr.s_addr = INADDR_ANY; /* Listen on every interface */
     tcp_server_data->server_address.sin_port = htons(port);
@@ -16,93 +16,91 @@ enum server_errors server_init(struct tcp_server *tcp_server_data, const int por
     int wsa_status = WSAStartup(MAKEWORD(2, 2), &tcp_server_data->wsa_data);
     if (wsa_status != 0)
     {
-        ret_val = SERVER_INIT_ERROR;
         int wsa_error = WSAGetLastError();
-        fprintf(stderr, "Failed to initialize Windows socket data. Error : \n", wsa_error);
-    }
-    else
-    {
-        tcp_server_data->server_socket = socket(AF_INET, SOCK_STREAM, 0);
-        if (tcp_server_data->server_socket == INVALID_SOCKET)
-        {
-            ret_val = SERVER_INIT_ERROR;
-            int wsa_error = WSAGetLastError();
-            fprintf(stderr, "Failed to create tcp server socket. Error : \n", wsa_error);
-        }
-        else
-        {
-            int bind_status = bind(tcp_server_data->server_socket, (struct sockaddr *)&tcp_server_data->server_address, sizeof(tcp_server_data->server_address));
-            if (bind_status == SOCKET_ERROR)
-            {
-                ret_val = SERVER_INIT_ERROR;
-                int wsa_error = WSAGetLastError();
-                fprintf(stderr, "Failed to bind server socket to the server address. Error : \n", wsa_error);
-            }
-            else
-            {
-                int listen_status = listen(tcp_server_data->server_socket, SOMAXCONN);
-                if (listen_status == SOCKET_ERROR)
-                {
-                    ret_val = SERVER_INIT_ERROR;
-                    int wsa_error = WSAGetLastError();
-                    fprintf(stderr, "Failed to listen for incoming connections. Error : \n", wsa_error);
-                }
-                else
-                {
-                    ret_val = SERVER_NO_ERROR;
-                    tcp_server_data->init_done = true;
-                    printf("TCP server socket initialization is succesfull.\n");
-                }
-            }
-        }
+        fprintf(stderr, "Port : %d Failed to initialize Windows socket data. Error : %d\n", ntohs(tcp_server_data->server_address.sin_port), wsa_error);
+        return SERVER_INIT_ERROR;
     }
 
-    return ret_val;
+    tcp_server_data->server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (tcp_server_data->server_socket == INVALID_SOCKET)
+    {
+        int wsa_error = WSAGetLastError();
+        fprintf(stderr, "Port : %d Failed to create server socket. Error : %d\n", ntohs(tcp_server_data->server_address.sin_port), wsa_error);
+        return SERVER_INIT_ERROR;
+    }
+
+    int bind_status = bind(tcp_server_data->server_socket, (struct sockaddr *)&tcp_server_data->server_address, sizeof(tcp_server_data->server_address));
+    if (bind_status == SOCKET_ERROR)
+    {
+        int wsa_error = WSAGetLastError();
+        fprintf(stderr, "Port : %d Failed to bind server socket to the server address. Error : %d\n", ntohs(tcp_server_data->server_address.sin_port), wsa_error);
+        return SERVER_INIT_ERROR;
+    }
+
+    int listen_status = listen(tcp_server_data->server_socket, SOMAXCONN);
+    if (listen_status == SOCKET_ERROR)
+    {
+        int wsa_error = WSAGetLastError();
+        fprintf(stderr, "Port : %d Failed to listen for incoming connections. Error : %d\n", ntohs(tcp_server_data->server_address.sin_port), wsa_error);
+        return SERVER_INIT_ERROR;
+    }
+
+    tcp_server_data->init_done = true;
+    printf("Port : %d TCP server socket initialization is succesfull.\n", ntohs(tcp_server_data->server_address.sin_port));
+    return SERVER_NO_ERROR;
 }
 
 enum server_errors server_run(struct tcp_server *tcp_server_data)
 {
-    enum server_errors ret_val = SERVER_RUNTIME_ERROR;
-
     if (tcp_server_data->init_done == false)
     {
-        ret_val = SERVER_RUNTIME_ERROR;
-        printf("TCP server is not initialized.\n");
+        printf("Port : %d TCP server is not initialized.\n", ntohs(tcp_server_data->server_address.sin_port));
+        return SERVER_RUNTIME_ERROR;
     }
-    else
+
+    pthread_t thread;
+    int thread_status = pthread_create(&thread, NULL, &server_task, (void *)tcp_server_data);
+    if (thread_status != 0)
     {
-        ret_val = SERVER_NO_ERROR;
-        printf("TCP server started listening on port 8888.\n");
+        fprintf(stderr, "Error creating server thread.");
+        return SERVER_RUNTIME_ERROR;
+    }
 
-        while (ret_val == SERVER_NO_ERROR)
+    return SERVER_NO_ERROR;
+}
+
+void *server_task(void *arg)
+{
+    struct tcp_server *tcp_server_data = (struct tcp_server *)arg;
+
+    printf("TCP server started listening on port : %d.\n", ntohs(tcp_server_data->server_address.sin_port));
+
+    while (true)
+    {
+        size_t client_sock_addr_len = sizeof(tcp_server_data->client_address);
+        tcp_server_data->client_socket = accept(tcp_server_data->server_socket, (struct sockaddr *)&tcp_server_data->client_address, (int *)&client_sock_addr_len);
+
+        if (tcp_server_data->client_socket == INVALID_SOCKET)
         {
-            size_t client_sock_addr_len = sizeof(tcp_server_data->client_address);
-            tcp_server_data->client_socket = accept(tcp_server_data->server_socket, (struct sockaddr *)&tcp_server_data->client_address, (int *)&client_sock_addr_len);
-
-            if (tcp_server_data->client_socket == INVALID_SOCKET)
+            fprintf(stderr, "Failed to accept client connection. Client socket : %d\n", tcp_server_data->client_socket);
+        }
+        else
+        {
+            printf("Client is connected. Client socket : %d\n", tcp_server_data->client_socket);
+            pthread_t thread;
+            int thread_status = pthread_create(&thread, NULL, &handle_client, (void *)tcp_server_data->client_socket);
+            if (thread_status != 0)
             {
-                fprintf(stderr, "Failed to accept client connection. Client socket : %d\n", tcp_server_data->client_socket);
+                fprintf(stderr, "Error creating client handle thread. Client socket : %d\n", tcp_server_data->client_socket);
+                break;
             }
             else
             {
-                printf("Client is connected. Client socket : %d\n", tcp_server_data->client_socket);
-                pthread_t thread;
-                int thread_status = pthread_create(&thread, NULL, &handle_client, (void *)tcp_server_data->client_socket);
-                if (thread_status != 0)
-                {
-                    ret_val = SERVER_RUNTIME_ERROR;
-                    fprintf(stderr, "Error creating client handle thread. Client socket : %d\n", tcp_server_data->client_socket);
-                }
-                else
-                {
-                    printf("New thread is created. Client socket : %d\n", tcp_server_data->client_socket);
-                }
+                printf("New thread is created. Client socket : %d\n", tcp_server_data->client_socket);
             }
         }
-
-        closesocket(tcp_server_data->server_socket);
-        WSACleanup();
     }
 
-    return ret_val;
+    closesocket(tcp_server_data->server_socket);
+    WSACleanup();
 }
